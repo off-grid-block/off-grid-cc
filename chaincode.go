c93535beafd161540a4baf146215e17529e1ede5
amudvari <48368732+amudvari@users.noreply.github.com>
2019-07-05 11:59:22 -0400

    Update chaincode.go

--- chaincode.go
+++ chaincode.go
@@ -1,10 +1,9 @@
 package main
 
-
-//not forked, offline edit 1
 //besides the normal packages, we need to import shim and peer
 //which can be found within fabric (git)
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"strconv"
@@ -13,8 +12,8 @@ import (
 	"github.com/hyperledger/fabric/protos/peer"
 )
 
-// we implement chaincode under 'ourchain' structure
-type ourchain struct {
+// we implement chaincode under 'ourChain' structure
+type ourChain struct {
 }
 
 // we represent metadata with 'ourdata' structure
@@ -28,30 +27,32 @@ type entry struct {
 	Updated     int    `json:"updated"`     //0 is outdated, 1 for updated
 }
 
-//main innitiate smartcontract ourchain
+//main innitiate smartcontract ourChain
 func main() {
-	err := shim.Start(new(ourchain))
+	err := shim.Start(new(ourChain))
 	if err != nil {
 		fmt.Print("Error starting the chaincode, reason: %s", err)
 	}
 }
 
 //Initialize the ledger
-func (v *ourchain) Init(stub shim.ChaincodeStubInterface) peer.Response {
+func (v *ourChain) Init(stub shim.ChaincodeStubInterface) peer.Response {
 	return shim.Success(nil)
 }
 
 //invoke a given function
-func (v *ourchain) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+func (v *ourChain) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	function, arguments := stub.GetFunctionAndParameters()
 	fmt.Println("function is running....")
 
-	if function == "initentry" {
-		return v.initentry(stub, arguments)
+	if function == "initEntry" {
+		return v.initEntry(stub, arguments)
 		//} else if function == "setentry" {
 		//return v.setentry(stub, arguments)
-	} else if function == "readentry" {
-		return v.readentry(stub, arguments)
+	} else if function == "readEntry" {
+		return v.readEntry(stub, arguments)
+	} else if function == "searchByOwner" {
+		return v.searchByOwner(stub, arguments)
 	}
 
 	fmt.Println("invoke did not find func: " + function)
@@ -59,7 +60,7 @@ func (v *ourchain) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 }
 
 //innitialize chaincode entry
-func (v *ourchain) initentry(stub shim.ChaincodeStubInterface, arguments []string) peer.Response {
+func (v *ourChain) initEntry(stub shim.ChaincodeStubInterface, arguments []string) peer.Response {
 
 	//detect errors in entry
 	var err error
@@ -92,16 +93,16 @@ func (v *ourchain) initentry(stub shim.ChaincodeStubInterface, arguments []strin
 	updated, err := strconv.Atoi(arguments[5])
 
 	//check if entry already exists
-	entrybytes, err := stub.GetState(id)
+	entryBytes, err := stub.GetState(id)
 	if err != nil {
 		return shim.Error("Fail to get entry: " + err.Error())
-	} else if entrybytes != nil {
+	} else if entryBytes != nil {
 		return shim.Error("This entry already exists.")
 	}
 
 	//create entry and marshal to JSON
-	objecttype := "entry"
-	entry := &entry{objecttype, id, hash, application, node, owner, updated}
+	objectType := "entry"
+	entry := &entry{objectType, id, hash, application, node, owner, updated}
 	entryJSONbytes, err := json.Marshal(entry)
 	if err != nil {
 		return shim.Error(err.Error())
@@ -113,42 +114,146 @@ func (v *ourchain) initentry(stub shim.ChaincodeStubInterface, arguments []strin
 		return shim.Error(err.Error())
 	}
 
-	//success message
+	//composite key
+	indices := "application-owner"
+	indicesKey, err := stub.CreateCompositeKey(indices, []string{entry.Application, entry.Owner})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	//save the indices entry to a state
+	value := []byte{0x00}
+	stub.PutState(indicesKey, value)
+
+	//return success
 	fmt.Println("end initiation")
 	return shim.Success(nil)
 }
 
 //read an entry into the ledger
-func (v *ourchain) readentry(stub shim.ChaincodeStubInterface, arguments []string) peer.Response {
-	var id, jsonResp string
+func (v *ourChain) readEntry(stub shim.ChaincodeStubInterface, arguments []string) peer.Response {
+	var id, jsonResponse string
 	var err error
 
+	//check for length
 	if len(arguments) != 1 {
 		return shim.Error("Incorrect number of arguments. expecting ID")
 	}
+
+	//get the record for corresponding ID
 	id = arguments[0]
 	entrybytes, err := stub.GetState(id)
 	if err != nil {
-		jsonResp = "failed to get entry"
-		return shim.Error(jsonResp)
+		jsonResponse = "failed to get entry"
+		return shim.Error(jsonResponse)
 	} else if entrybytes == nil {
-		jsonResp = "No entry for " + id + " was found"
-		return shim.Error(jsonResp)
+		jsonResponse = "No entry for " + id + " was found"
+		return shim.Error(jsonResponse)
 	}
 	return shim.Success(entrybytes)
 }
 
-/*
-package main
+//delete an entry from the state
+func (v *ourChain) deleteEntry(stub shim.ChaincodeStubInterface, arguments []string) peer.Response {
+	var jsonResponse string
+	var entryJSON entry
 
-import (
-	ft "fmt"
-)
+	if len(arguments) != 1 {
+		return shim.Error("Incorrect number of arguments")
+	}
 
-func main() {
-	ab := 1
-	Ab := 2
-	ft.Println(ab)
-	ft.Println(Ab)
+	//obtain the state:
+	entry := arguments[0]
+	entrybytes, err := stub.GetState(entry)
+	if err != nil {
+		jsonResponse = "failed to get entry"
+		return shim.Error(jsonResponse)
+	} else if entrybytes == nil {
+		jsonResponse = "No entry for " + entry + " was found"
+		return shim.Error(jsonResponse)
+	}
+
+	err = json.Unmarshal([]byte(entrybytes), &entryJSON) //unmarshall
+	if err != nil {
+		jsonResponse = "{\"Error\":\"Failed to get state for " + entry + "\"}"
+		return shim.Error(jsonResponse)
+	}
+
+	err = stub.DelState(entry) //delete
+	if err != nil {
+		return shim.Error("Failed to delete the state")
+	}
+
+	return shim.Success(nil)
 }
-*/
+
+//get all entry for an owner
+func (v *ourChain) searchByOwner(stub shim.ChaincodeStubInterface, arguments []string) peer.Response {
+
+	//check the length to insure it is 1
+	if len(arguments) != 1 {
+		return shim.Error("Incorrect number of arguments. expecting owner")
+	}
+
+	//json string
+	owner := arguments[0]
+	jsonString := fmt.Sprintf("{\"selector\":{\"docType\":\"entry\",\"owner\":\"%s\"}}", owner)
+
+	//call fuction that gets results for the corresponding string
+	results, err := resultFromString(stub, jsonString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(results)
+}
+
+//get
+
+//get query result from string is obtained and passed as byte array
+func resultFromString(stub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+
+	//get iterator
+	result, err := stub.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+
+	//construct query result from iterator
+	buffer, err := constructQueryResponse(result)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Query result: %s\n", buffer)
+	return buffer.Bytes(), nil
+}
+
+//get records in byte format from iterator
+func constructQueryResponse(result shim.StateQueryIteratorInterface) (*bytes.Buffer, error) {
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	writtenarray := false
+	for result.HasNext() {
+		queryResponse, err := result.Next()
+		if err != nil {
+			return nil, err
+		}
+		if writtenarray == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(",\"Record\":")
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		writtenarray = true
+	}
+	buffer.WriteString("]")
+
+	return &buffer, nil
+}
